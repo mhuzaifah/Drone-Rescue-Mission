606039f389ac54f0f429b7ba660cf504d0ecef4b
Haniya Kashif <haniya.kashif2801@gmail.com>
2024-03-22 03:59:29 -0400

    Set up of the coordinates class

--- src/main/java/ca/mcmaster/se2aa4/island/team119/MapCoordinate.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team119/MapCoordinate.java
@@ -1,17 +1,46 @@
 package ca.mcmaster.se2aa4.island.team119;
 
 public class MapCoordinate {
+    private int x;
+    private int y;
 
-    Integer x;
-    Integer y;
+    public MapCoordinate() {}
 
-    MapCoordinate(Integer xCor, Integer yCor){
+    /*MapCoordinate(Integer xCor, Integer yCor){
         this.x = xCor;
         this.y = yCor;
+    }*/
+
+    public int getX() {
+        return x;
     }
 
-    public void translate(Integer range, Direction droneHeading) {
-        x++;
-        y++;
+    public int getY() {
+        return y;
+    }
+
+    public void droneTranslate(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MapCoordinate other = (MapCoordinate) obj;
+        return x == other.x && y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Integer.hashCode(x);
+        result = 31 * result + Integer.hashCode(y);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
     }
 }
